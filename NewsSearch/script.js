71684fec73c04d343df72df71520839b851d4340
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-03-17 17:45:56 +0530

    add new codes

--- NewsSearch/script.js
+++ NewsSearch/script.js
@@ -5,5 +5,67 @@ var arr = [
     {name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
     {name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     {name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+    {name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: " Cold drink", image: "https://images.unsplash.com/photo-1544145945-f90425340c7e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name: "don in street", image: "https://plus.unsplash.com/premium_photo-1663126581582-a1f900083da6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
 ]
+
+   let mainBox = document.querySelector(".container")
+
+function showTheCards() {
+    var box = "";
+    // forEach to iterate over each object in the array
+    arr.forEach((obj) => {
+        box += `<div class="box">
+            <img class="cursor-pointer" src="${obj.image}" alt="image">
+            <div class="caption"> glo </div>
+        </div>`;
+    });
+
+    mainBox.innerHTML = box ;
+    // console.log(box);
+}
+      
+    function handlSearchFuntionality () {
+
+           var input =document.querySelector ("#searchinput") 
+        input.addEventListener("focus" , function () {
+             document.querySelector(".overlay").style.display = "block" ;
+        })
+
+        input.addEventListener("blur" , function () {
+            document.querySelector(".overlay").style.display = "none" ;
+       });
+
+
+
+
+    //   for search value 
+
+      input.addEventListener ("input" , function () {
+     const filteredArray = arr.filter((obj) => obj.name.toLowerCase().startsWith(input.value));
+     
+  var clutter = " " ;
+    filteredArray.forEach(function (obj) {
+        clutter += ` <div class = "res flex px-8 py-3"> 
+        <i class = "ri-search-line font-semibold mr-5"> </i> 
+        <h3 class ="font-semibold "> ${obj.name} </h3>
+        </div> `
+    })
+         document.querySelector(".searchdata").style.display ="block";
+
+         document.querySelector(".searchdata").innerHTML = clutter
+        
+
+    //   console.log(filteredArray);
+    
+    } )
+
+
+
+
+
+    }
+
+    handlSearchFuntionality ();
+showTheCards();
